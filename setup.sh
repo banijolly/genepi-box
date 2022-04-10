9a55979e33801ba1a432642548b957ef3e6a2e3b
banijolly <47384613+banijolly@users.noreply.github.com>
2022-04-10 16:27:58 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,6 +20,6 @@ pip install --user ./gisaid_cli2
 cp ~/.local/bin/cli2 ~/anaconda3/envs/genepi-box/bin/
 
 cd ..
-cp scripts/php.ini ~/anaconda3/envs/genepi-box/lib/php/
+cp scripts/php.ini ~/anaconda3/envs/genepi-box/lib
 
 echo "Enviroment set up complete"
